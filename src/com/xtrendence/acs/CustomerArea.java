27df8b9384f2b10f1509c3f9409aeebe62085f67
Xtrendence <xtrendence@yahoo.com>
2020-12-19 02:44:59 +0000

    Added a popup menu when a JTable row is right-clicked.

--- src/com/xtrendence/acs/CustomerArea.java
+++ src/com/xtrendence/acs/CustomerArea.java
@@ -1,11 +1,15 @@
 package com.xtrendence.acs;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -44,8 +48,11 @@ public class CustomerArea extends JFrame {
         itemTable.setForeground(new Color(75,75,75));
         itemTable.setSelectionBackground(new Color(0,125,255));
         itemTable.setSelectionForeground(new Color(255,255,255));
+        itemTable.setGridColor(new Color(230,230,230));
         itemTable.getTableHeader().setPreferredSize(new Dimension(itemTable.getTableHeader().getWidth(), 30));
         itemTable.setRowHeight(30);
+        itemTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        itemTable.setDefaultEditor(Object.class, null);
     }
 
     public static void main(String[] args) throws IOException {
@@ -58,6 +65,43 @@ public class CustomerArea extends JFrame {
 
         customerArea.setVisible(true);
         customerArea.setContentPane(customerArea.mainPanel);
+
+        JPopupMenu popupMenu = new JPopupMenu();
+        JMenuItem scanItem = new JMenuItem("Scan Item");
+        scanItem.setSize(scanItem.getWidth(), 30);
+        scanItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String code = customerArea.itemTable.getValueAt(customerArea.itemTable.getSelectedRow(), 0).toString();
+            }
+        });
+        popupMenu.add(scanItem);
+        customerArea.itemTable.setComponentPopupMenu(popupMenu);
+
+        popupMenu.addPopupMenuListener(new PopupMenuListener() {
+            @Override
+            public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        int rowAtCursor = customerArea.itemTable.rowAtPoint(SwingUtilities.convertPoint(popupMenu, new Point(0, 0), customerArea.itemTable));
+                        if(rowAtCursor > -1) {
+                            customerArea.itemTable.setRowSelectionInterval(rowAtCursor, rowAtCursor);
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent popupMenuEvent) {
+
+            }
+
+            @Override
+            public void popupMenuCanceled(PopupMenuEvent popupMenuEvent) {
+
+            }
+        });
     }
 
     public static void updateTable(List<Item> stock, JTable table) {
