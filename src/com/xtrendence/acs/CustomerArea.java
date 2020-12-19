7825aafac96b6131c6af8c640c081eccffb72af5
Xtrendence <xtrendence@yahoo.com>
2020-12-19 00:11:42 +0000

    Added a JTable.

--- src/com/xtrendence/acs/CustomerArea.java
+++ src/com/xtrendence/acs/CustomerArea.java
@@ -1,6 +1,10 @@
 package com.xtrendence.acs;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -14,6 +18,7 @@ public class CustomerArea extends JFrame {
     private JPanel actionsWrapper;
     private JLabel basketButton;
     private JLabel adminButton;
+    private JTable itemTable;
 
     public CustomerArea() throws IOException {
         this.setSize(1280, 720);
@@ -24,19 +29,32 @@ public class CustomerArea extends JFrame {
         navbar.setBackground(new Color(0, 125, 255));
         navbar.setSize(navbar.getWidth(), 60);
 
+        mainPanel.setBackground(new Color(245,245,245));
+
         BufferedImage basketIcon = ImageIO.read(new File(System.getProperty("user.dir") + "\\resources\\basket.png"));
         basketButton.setIcon(new ImageIcon(basketIcon.getScaledInstance(40, 40, Image.SCALE_SMOOTH)));
 
         BufferedImage userIcon = ImageIO.read(new File(System.getProperty("user.dir") + "\\resources\\user.png"));
         adminButton.setIcon(new ImageIcon(userIcon.getScaledInstance(40, 40, Image.SCALE_SMOOTH)));
+
+        itemTable.setBackground(new Color(255, 255, 255));
+        itemTable.setForeground(new Color(75,75,75));
     }
 
     public static void main(String[] args) throws IOException {
         CustomerArea customerArea = new CustomerArea();
-        customerArea.setVisible(true);
-        customerArea.setContentPane(customerArea.mainPanel);
 
         Stock stock = new Stock();
         stock.updateStock();
+
+        String[] columns = new String[]{ "Product Code", "Name", "Price", "Remaining Quantity" };
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(columns);
+
+        customerArea.itemTable.setModel(model);
+        customerArea.itemTable.repaint();
+
+        customerArea.setVisible(true);
+        customerArea.setContentPane(customerArea.mainPanel);
     }
 }
